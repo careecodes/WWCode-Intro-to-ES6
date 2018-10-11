a514fa2e3076be1998886fc6d77b29e784e56ee2
Caree Claunch <caree@caree.codes>
2018-10-10 19:39:21 -0500

    example of js class and inheritance

---
+++ classes-and-inheritance/script.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+
+class HonorStudent extends Student {
+    
+}
